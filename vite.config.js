2479ca22f66af7c59bfe5737952cda7387394de2
EliteGoodDev <michaeljohnares.1026@gmail.com>
2025-04-21 05:16:59 -0500

    initialize project.

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  define: {
+    'process.env': {},
+    global: 'window',
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer/',
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+    },
+  },
+})
